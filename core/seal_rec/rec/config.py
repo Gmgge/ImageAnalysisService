b64d00a75c6ad319ac81be7c5bb2da323f3dcbc8
GM <157505853@qq.com>
2024-04-30 22:03:48 +0800

    upgrade: onnx推理已经兼容新版本，更新默认模型路径

--- core/seal_rec/rec/config.py
+++ core/seal_rec/rec/config.py
@@ -5,7 +5,7 @@ from conf.service_args import project_root
 
 def init_rec_args():
     args = AnalysisArgs()
-    args.model_path = os.path.join(project_root, "data/seal/rec_torch")
+    args.model_path = os.path.join(project_root, "data/seal/recognition")
     args.threshold = 0.6  # 置信度阈值，由于未进行负样本训练，该阈值较高
     args.max_len = 50  # 最长文本长度
     args.input_shape = (384, 384)  # 模式输入图像尺度
